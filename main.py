f7c9849f0e30e482032c63c6cb693ba81a84884c
alanyumbo123 <aj.yumbog@uea.edu.ec>
2026-01-18 21:23:57 -0500

    Archivo principal

---
+++ main.py
@@ -0,0 +1,10 @@
+# Programa principal
+# Autor: Alan Yumbo
+
+from servicios.gestion_animales import mostrar_info_animal
+
+def main():
+    mostrar_info_animal()
+
+if __name__ == "__main__":
+    main()
